384bd98801cff308c5f4da81fc2943bd2821587f
Chithma Dilmin Pathirana <chithmadilmina@gmail.com>
2025-04-23 23:39:15 +0530

    Create dashboard
    Create UI,
    Create backend
    Create data source

---
+++ ASM-System_CGP/ASM-System_CGP/SystemDashboard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASM_System_CGP
+{
+    public partial class SystemDashboard : Form
+    {
+        public SystemDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            Show_Vehicles show = new Show_Vehicles();
+            show.ShowDialog();
+        }
+    }
+}
